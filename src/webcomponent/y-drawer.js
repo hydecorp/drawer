5ef34ae8922adcfcc5a8a8717c82e64211b199a5
Florian Klampfer <f.klampfer@gmail.com>
2016-10-11 11:19:53 +0200

    Make importable in IE

--- src/webcomponent/y-drawer.js
+++ src/webcomponent/y-drawer.js
@@ -8,4 +8,9 @@ import htmlElement from 'y-component/src/html-element';
 
 import drawerCore from '../core';
 
+// Older browsers don't treat `HTMLElement` as a class,
+// which would cause an exception from just importing the class below (even when never using it)
+// Replacing it with a dummy function prevents this.
+const HTMLElement = typeof global.HTMLElement === 'function' ? global.HTMLElement : () => {};
+
 export default class extends htmlElement(drawerCore(HTMLElement)) {}
