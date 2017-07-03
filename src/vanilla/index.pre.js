de42b4675bf9af8e766f805c5a8945452124a287
Florian Klampfer <mail@qwtel.com>
2017-07-03 15:35:03 +0200

    copyright consistency

--- src/vanilla/index.pre.js
+++ src/vanilla/index.pre.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2017  Florian Klampfer
+// Copyright (c) 2017 Florian Klampfer
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,7 +20,6 @@ const style = `
 <style>
 <!-- @include ../drawer.css -->
 </style>`;
-
 
 function fragmentFromString(strHTML) {
   return document.createRange().createContextualFragment(strHTML);
--- src/webcomponent/y-drawer.js
+++ src/webcomponent/y-drawer.js
@@ -1,4 +1,4 @@
-// y-drawer  Copyright (C) 2017  Florian Klampfer
+// Copyright (c) 2017 Florian Klampfer
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
