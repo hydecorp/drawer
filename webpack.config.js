5a894dfa408a63056bf003ed5e8cf7dce349de5c
Florian Klampfer <mail@qwtel.com>
2018-03-08 11:34:47 +0000

    move header to file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { readFileSync } = require('fs');
 const { resolve } = require('path');
 
 const {
@@ -19,22 +20,7 @@ const { name: filename } = require('./package.json');
 
 const min = env === 'lite' ? '-lite.min' : env === 'prod' ? '.min' : ''; // eslint-disable-line
 const library = camelcase(filename);
-const banner = String.prototype.trim.call(`
-Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-`);
+const banner = readFileSync(resolve('./header.md'), 'utf-8');
 
 const flatten = [(a, x) => a.concat(x), []];
 
