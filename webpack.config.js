5a816658748a0f711b958861a48384ddfe73641c
Florian Klampfer <mail@qwtel.com>
2018-03-24 21:43:16 +0000

    add source map for minified build (.map file)

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,85 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
 
 const {
   BannerPlugin,
   EnvironmentPlugin,
-  optimize: { UglifyJsPlugin, ModuleConcatenationPlugin },
-} = require('webpack');
+  optimize: { UglifyJsPlugin, ModuleConcatenationPlugin }
+} = require("webpack");
 
-const merge = require('webpack-merge');
-const { argv: { env } } = require('yargs');
-const camelcase = require('camelcase');
+const merge = require("webpack-merge");
+const { argv: { env } } = require("yargs");
+const camelcase = require("camelcase");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { name: filename } = require('./package.json');
+const { name: filename } = require("./package.json");
 
-const min = env === 'prod' ? '.min' : '';
+const min = env === "prod" ? "" : ".dev";
 const library = camelcase(filename);
-const banner = readFileSync(resolve('./header.txt'), 'utf-8');
+const banner = readFileSync(resolve("./header.txt"), "utf-8");
 
 const flatten = [(a, x) => a.concat(x), []];
 
 function envConfig() {
   switch (env) {
-    case 'prod':
+    case "prod":
       return {
         plugins: [
           new BannerPlugin({ banner }),
           new EnvironmentPlugin({ DEBUG: false }),
-          new UglifyJsPlugin(),
-        ],
+          new UglifyJsPlugin({ sourceMap: true })
+        ]
       };
 
     default:
       return {
-        devtool: 'source-map',
-        plugins: [new EnvironmentPlugin({ DEBUG: true })],
+        plugins: [new EnvironmentPlugin({ DEBUG: true })]
       };
   }
 }
 
 const baseConfig = merge(
   {
+    devtool: "source-map",
     output: {
       filename: `index${min}.js`,
       library,
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
+      libraryTarget: "umd",
+      umdNamedDefine: true
     },
     module: {
       rules: [
         {
           test: /(\.jsx|\.js)$/,
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [['env', { modules: false }]],
-            babelrc: false,
-          },
+            presets: [["env", { modules: false }]],
+            babelrc: false
+          }
         },
         {
           test: /\.html$/,
-          use: 'raw-loader',
+          use: "raw-loader"
         },
         {
           test: /\.ejs$/,
-          loader: 'underscore-template-loader',
-        },
-      ],
+          loader: "underscore-template-loader"
+        }
+      ]
     },
     resolve: {
       modules: [
-        resolve('./src'),
-        resolve('./node_modules'),
-        process.env.NODE_PATH ? resolve(process.env.NODE_PATH) : [],
+        resolve("./src"),
+        resolve("./node_modules"),
+        process.env.NODE_PATH ? resolve(process.env.NODE_PATH) : []
       ].reduce(...flatten),
-      extensions: ['.json', '.js'],
-      symlinks: true,
+      extensions: [".json", ".js"],
+      symlinks: true
     },
-    plugins: [new ModuleConcatenationPlugin()],
+    plugins: [new ModuleConcatenationPlugin()]
   },
-  envConfig(),
+  envConfig()
 );
 
 const cssRawLoaderConfig = {
@@ -87,73 +87,73 @@ const cssRawLoaderConfig = {
     rules: [
       {
         test: /\.css$/,
-        loader: 'raw-loader',
-      },
-    ],
-  },
+        loader: "raw-loader"
+      }
+    ]
+  }
 };
 
 const config = [
   // Mixin
   merge(baseConfig, {
-    entry: resolve('./src/mixin/index.js'),
+    entry: resolve("./src/mixin/index.js"),
     output: {
-      path: resolve('./dist/mixin'),
-    },
+      path: resolve("./dist/mixin")
+    }
   }),
 
   // Vanilla JS
   merge(baseConfig, {
-    entry: resolve('./src/vanilla/index.js'),
+    entry: resolve("./src/vanilla/index.js"),
     output: {
-      path: resolve('./dist/vanilla'),
-    },
+      path: resolve("./dist/vanilla")
+    }
   }),
 
   // jQuery
   merge(baseConfig, {
-    entry: resolve('./src/jquery/index.js'),
+    entry: resolve("./src/jquery/index.js"),
     output: {
-      path: resolve('./dist/jquery'),
+      path: resolve("./dist/jquery")
     },
     externals: [
       {
-        jquery: 'jQuery',
-      },
-    ],
+        jquery: "jQuery"
+      }
+    ]
   }),
 
   // WebComponent Standalone
   merge(baseConfig, {
-    entry: resolve('./src/webcomponent/index.js'),
+    entry: resolve("./src/webcomponent/index.js"),
     output: {
-      path: resolve('./dist/webcomponent'),
-    },
+      path: resolve("./dist/webcomponent")
+    }
   }),
 
   // WebComponent Module
   merge(baseConfig, {
-    entry: resolve('./src/webcomponent/module.js'),
+    entry: resolve("./src/webcomponent/module.js"),
     output: {
-      path: resolve('./dist/webcomponent'),
-      filename: `module${min}.js`,
-    },
+      path: resolve("./dist/webcomponent"),
+      filename: `module${min}.js`
+    }
   }),
 
   // WebComponent HTML Import
   merge(baseConfig, cssRawLoaderConfig, {
-    entry: resolve('./src/webcomponent/html-import.js'),
+    entry: resolve("./src/webcomponent/html-import.js"),
     output: {
-      path: resolve('./dist/webcomponent'),
-      filename: `html-import${min}.js`,
+      path: resolve("./dist/webcomponent"),
+      filename: `html-import${min}.js`
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: resolve('./src/webcomponent/index.ejs'),
-        filename: `${filename}${min}.html`,
-      }),
-    ],
-  }),
+        template: resolve("./src/webcomponent/index.ejs"),
+        filename: `${filename}${min}.html`
+      })
+    ]
+  })
 ];
 
 module.exports = config;
