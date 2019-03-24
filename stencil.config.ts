b11732ddd21b29e118532f30d1d543bfe8cbe21a
Florian Klampfer <mail@qwtel.com>
2019-03-24 15:26:04 +0700

    Update

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,8 +3,10 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'hy-drawer',
+  minifyJs: false,
   outputTargets: [
     { type: 'dist' },
+    { type: 'www' },
     { type: 'docs' },
     // { type: 'www' },
   ],
