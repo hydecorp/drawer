893b4fd23e9abd661c6507c74cdbdaff02fdeccb
Florian Klampfer <mail@qwtel.com>
2019-08-03 22:08:00 +0700

    Separate styels

--- src/components/hy-drawer/index.ts
+++ src/components/hy-drawer/index.ts
@@ -17,7 +17,7 @@
  * @license 
  * @nocompile
  */
-import { LitElement, html, css, property, customElement, query } from 'lit-element';
+import { LitElement, html, property, customElement, query } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 import { styleMap } from 'lit-html/directives/style-map';
 
@@ -30,6 +30,7 @@ import { applyMixins, filterWhen, easeOutSine, observeWidth } from './common';
 import { ObservablesMixin, Coord } from './observables';
 import { CalcMixin } from './calc';
 import { UpdateMixin, Updater } from './update';
+import { styles } from './styles';
 
 class RxLitElement extends LitElement {
   $connected = new Subject<boolean>();
@@ -62,7 +63,9 @@ export class HyDrawer
     extends applyMixins(RxLitElement, [ObservablesMixin, UpdateMixin, CalcMixin])
     implements ObservablesMixin, UpdateMixin, CalcMixin {
 
-  el: HTMLElement = this
+  static styles = styles;
+
+  el: HTMLElement = this;
 
   @query('.scrim') scrimEl: HTMLElement;
   @query('.content') contentEl: HTMLElement;
@@ -403,100 +406,4 @@ export class HyDrawer
   toggle() {
     this.animateTo$.next(!this.opened);
   }
-
-  static styles = css`
-    @media screen {
-      :host {
-        touch-action: pan-x;
-      }
-
-      .span-screen {
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100vh;
-        width: 100vw;
-      }
-
-      .span-height {
-        position: fixed;
-        top: 0;
-        height: 100vh;
-      }
-
-      .peek {
-        left: 0;
-        width: var(--hy-drawer-peek-width, 0px);
-        visibility: hidden;
-        z-index: calc(var(--hy-drawer-z-index, 100) - 1);
-      }
-
-      .scrim {
-        opacity: 0;
-        pointer-events: none;
-        background: var(--hy-drawer-scrim-background, rgba(0, 0, 0, 0.5));
-        z-index: var(--hy-drawer-z-index, 100);
-        -webkit-tap-highlight-color: transparent;
-      }
-
-      .range {
-        position: fixed;
-        top: 0;
-        height: 100vh;
-        z-index: calc(var(--hy-drawer-z-index, 100) + 1);
-      }
-
-      .grabbing-screen {
-        cursor: grabbing;
-        z-index: calc(var(--hy-drawer-z-index, 100) + 2);
-      }
-
-      .content {
-        width: var(--hy-drawer-width, 300px);
-        background: var(--hy-drawer-background, inherit);
-        box-shadow: var(--hy-drawer-box-shadow, 0 0 15px rgba(0, 0, 0, 0.25));
-        z-index: calc(var(--hy-drawer-z-index, 100) + 3);
-        contain: strict;
-      }
-
-      .content.left {
-        left:  calc(-1 * var(--hy-drawer-width, 300px) + var(--hy-drawer-peek-width, 0px));
-      }
-
-      .content.right {
-        right:  calc(-1 * var(--hy-drawer-width, 300px) + var(--hy-drawer-peek-width, 0px));
-      }
-
-      .content > .overflow {
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        overflow-x: hidden;
-        overflow-y: auto;
-        will-change: scroll-position;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .grab {
-        cursor: move;
-        cursor: grab;
-      }
-
-      .grabbing {
-        cursor: grabbing;
-      }
-    }
-
-    @media print {
-      .scrim {
-        display: none !important;
-      }
-
-      .content {
-        transform: none !important;
-      }
-    }
-  `;
 }
