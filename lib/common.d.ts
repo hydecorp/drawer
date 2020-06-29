b7306225521733d26583a45f60e2472a67e81aa8
Florian Klampfer <mail@qwtel.com>
2020-06-29 14:57:21 +0700

    Restore preversion .gitignore

--- lib/common.d.ts
+++
@@ -1,9 +0,0 @@
-export { applyMixins, subscribeWhen, filterWhen, tween } from '@hydecorp/component';
-export declare function easeOutSine(t: number, b: number, c: number, d: number): number;
-export declare function observeWidth(el: HTMLElement): import("rxjs").Observable<number>;
-export declare const rangeConverter: {
-    fromAttribute(attr?: string): number[];
-    toAttribute(range?: number[]): string;
-};
-export declare function rangeHasChanged(curr: number[], prev?: number[]): boolean;
-//# sourceMappingURL=common.d.ts.map
