8f264878afbe640518f4904cdd6ccfa057ab9277
Florian Klampfer <mail@qwtel.com>
2020-10-31 16:59:09 +0700

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
+export default [{
     input: 'src/index.ts',
-    output: {
+    output: [{
         file: `docs/assets/hy-drawer.js`,
         format: 'es',
         sourcemap: true
-    },
+    }, {
+        file: `module/index.js`,
+        format: 'es',
+        sourcemap: true
+    }],
     plugins: [
         typescript(),
         resolve(),
         commonjs(),
+        terser(),
     ],
-};
+}];
